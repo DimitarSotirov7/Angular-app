5630df0027bc5f2b0e59b68f05cd73b197db39de
Dimitar Sotirov <ABWJ4ZZ@mmm.com>
2021-08-03 15:42:45 +0300

    Add recent blogs

---
+++ Application/src/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  application: {
+    name: 'Sancho Panza'
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBlpN9_G6sZ4jY-fzswsDM7lCUWoJ8bjh4",
+    authDomain: "angular-app-fb611.firebaseapp.com",
+    projectId: "angular-app-fb611",
+    storageBucket: "angular-app-fb611.appspot.com",
+    messagingSenderId: "585872382003",
+    appId: "1:585872382003:web:fd989d8d86b2753a902b46",
+    measurementId: "G-TD3G7GL00R"
+  },
+  admin: {
+    email: 'sotirov.admin@gmail.com',
+  }
+};
